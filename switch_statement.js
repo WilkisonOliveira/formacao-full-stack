850a352f17aa1998f43fb40a4103f31d4ed3e521
WilkisonOliveira <viagrasilva52@gmail.com>
2026-09-14 18:26:00 -0300

    Atualiza projeto

---
+++ switch_statement.js
@@ -0,0 +1,17 @@
+//usado para +de um resultado, ao inves do so if e else
+
+var place = 'first';
+
+switch (place) {
+    case 'first':
+        console.log('Gold');
+        break;
+    case 'second':
+        console.log('Silver');
+        break;
+    case 'third':
+        console.log('Bronze');
+        break;
+    default:
+        console.log('No medal');
+}
