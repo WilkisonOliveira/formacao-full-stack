303f6d87538c73f505695cbd9c395cc2ac21f0ed
WilkisonOliveira <viagrasilva52@gmail.com>
2026-09-15 18:47:38 -0300

    Atualizar projeto

--- loop_while.js
+++ loop_while.js
@@ -1,3 +1,6 @@
+//Um loop while continua executando um bloco de código enquanto a condição especificada for avaliada como verdadeira.
+
+
 var counter = 3
 while (counter > 0) {
     console.log(counter);
