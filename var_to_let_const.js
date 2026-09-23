d268150b0937b029df3b5ddf7274cc4567161136
WilkisonOliveira <viagrasilva52@gmail.com>
2026-09-23 18:59:51 -0300

    Atualizar projeto

---
+++ var_to_let_const.js
@@ -0,0 +1,17 @@
+const x = 10;
+console.log(x);
+
+let a = 20;
+console.log(a);
+
+if (true) {
+    let z = 30;
+    console.log(z);
+}
+
+let b = 40;
+b = 50;
+console.log(b);
+
+const PI = 3.14159;
+console.log(PI);
